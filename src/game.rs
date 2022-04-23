50ebcbfdcadb0ec4a97ad72a79f500bb464a8718
artkonr <artkonr@gmail.com>
2022-04-23 03:14:32 +0300

    refactored game loop; added check for input length; added docs

--- src/game.rs
+++ src/game.rs
@@ -1,8 +1,104 @@
+use std::borrow::Borrow;
 use std::collections::{HashMap, HashSet};
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+use std::io::stdin;
 use std::iter::{Enumerate, repeat};
+use std::ops::Add;
 use std::str::Chars;
 use colored::{ColoredString, Colorize};
 
+/// Amount of attempts a user has
+/// to guess the secret word.
+const ATTEMPT_COUNT: u8 = 6;
+
+/// Game result: an empty tuple if
+/// the game loop terminated with
+/// a correct guess of a [Word];
+/// a [GameLost] error - if otherwise.
+pub type Result = std::result::Result<(), GameLost>;
+
+/// Runs the game loop until
+/// either the [Word] is guessed
+/// or the number of attempts
+/// reaches [ATTEMPT_COUNT].
+pub fn start_game_loop(word: &Word) -> Result {
+
+    let mut attempt_n = 0;
+    loop {
+
+        if attempt_n == ATTEMPT_COUNT {
+            return Result::Err(GameLost::with_word(word));
+        }
+
+        let mut input = String::new();
+        stdin()
+            .read_line(&mut input)
+            .expect("Failed to read user input");
+
+        let guess = String::from(input.trim_end());
+
+        if guess.len() != 5 {
+            println!("You'll need 5 characters to make it work!");
+            continue;
+        }
+
+        let result = word.try_match(&guess);
+
+        if result.full_match() {
+            println!(
+                "{} {}",
+                "You won!".green(),
+                format!("You needed {} attempts", attempt_n).normal()
+            );
+            return Result::Ok(());
+        } else {
+            result.print_result_for(&guess);
+            attempt_n = attempt_n.add(1);
+        }
+
+    }
+
+}
+
+
+/// A custom [Error] type that reports
+/// that a game is lost.
+#[derive(Debug)]
+pub struct GameLost {
+    secret: String
+}
+
+impl GameLost {
+    /// Constructs a new error object from some secret.
+    fn with_word(word: &Word) -> GameLost {
+        GameLost {
+            secret: word.reveal().clone()
+        }
+    }
+}
+
+impl Display for GameLost {
+
+    /// Prints out the message telling
+    /// that a game is lost :(.
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let str = format!(
+            "{} The word was '{}'",
+            "You lost :(".red(),
+            &self.secret
+        );
+        write!(f, "{}", str)
+    }
+}
+
+impl Error for GameLost {}
+
+
+/// **The word**, i.e. the secret word
+/// generated from the dictionary at
+/// the start of the game and which a
+/// user is supposed to guess.
 #[derive(Debug)]
 pub struct Word {
 
@@ -13,10 +109,17 @@ pub struct Word {
 
 impl Word {
 
+    /// Same as [Word::analyze], but accepts a `&str`.
     pub fn analyze_str(word: &str) -> Word {
         Word::analyze(String::from(word))
     }
 
+    /// Constructs a [Word] object out of a string.
+    ///
+    /// # Panics
+    /// * Will `panic!` if the string a [Word] is
+    ///   supposed to be constructed from is not
+    ///   precisely 5 characters long.
     pub fn analyze(word: String) -> Word {
         if word.len() != 5 {
             panic!("Secret word must be exactly 5 chars long, got {}", word)
@@ -43,6 +146,9 @@ impl Word {
         }
     }
 
+    /// Takes a string and checks it letter-by-letter
+    /// against the internally contained secret, thus
+    /// producing a [GuessResult].
     pub fn try_match(&self, word: &String) -> GuessResult {
         if self.val.eq(word) {
             return GuessResult::new_all_green();
@@ -56,24 +162,27 @@ impl Word {
                 .map(|hset| hset.contains(&(ind as u8)))
                 .map(|rs|
                     if rs {
-                        MatchResult::MATCH
+                        MatchResult::Match
                     } else {
-                        MatchResult::EXISTS
+                        MatchResult::Exists
                     }
                 )
-                .unwrap_or(MatchResult::NONE);
+                .unwrap_or(MatchResult::None);
             guess.push(match_result);
         }
 
         guess
     }
 
-    pub fn reveal(&self) -> &String {
+    /// Shows the secret word.
+    fn reveal(&self) -> &String {
         &self.val
     }
 
 }
 
+/// An in-loop stateful object that
+/// tracks letter matches.
 #[derive(Debug)]
 pub struct GuessResult {
     result: Vec<MatchResult>
@@ -81,12 +190,16 @@ pub struct GuessResult {
 
 impl GuessResult {
 
+    /// Returns `true` if all letters
+    /// and their positions have been
+    /// guessed correctly.
     pub fn full_match(&self) -> bool {
         let res = &self.result.iter()
-            .all(|item| *item == MatchResult::MATCH);
+            .all(|item| *item == MatchResult::Match);
         *res
     }
 
+    /// Pretty-prints the result of a guess attempt.
     pub fn print_result_for(&self, word: &String) {
         let mut chars = word.chars().enumerate();
 
@@ -100,6 +213,9 @@ impl GuessResult {
         );
     }
 
+    /// Takes next letter from the character
+    /// iterator and colors it according to
+    /// the [GuessResult].
     fn get_and_colorize(e: &mut Enumerate<Chars>, result: &GuessResult) -> ColoredString {
         let tuple = e.next().unwrap();
         let ch = tuple.1;
@@ -107,15 +223,17 @@ impl GuessResult {
             .get(tuple.0).unwrap();
 
         match res {
-            MatchResult::MATCH => String::from(ch).green().bold(),
-            MatchResult::EXISTS => String::from(ch).yellow().bold(),
-            MatchResult::NONE => String::from(ch).normal()
+            MatchResult::Match => String::from(ch).green().bold(),
+            MatchResult::Exists => String::from(ch).yellow().bold(),
+            MatchResult::None => String::from(ch).normal()
         }
     }
 
+    /// Creates a [GuessResult] that starts
+    /// with all buckets filled with [MatchResult::Match]
     fn new_all_green() -> GuessResult {
         let result = Vec::from_iter(
-            repeat(MatchResult::MATCH)
+            repeat(MatchResult::Match)
                 .take(5)
         );
         GuessResult {
@@ -123,37 +241,52 @@ impl GuessResult {
         }
     }
 
+    /// Creates an empty [GuessResult].
     fn new_empty() -> GuessResult {
         GuessResult { result: Vec::with_capacity(5) }
     }
 
+    /// Tracks a new letter [MatchResult].
     fn push(&mut self, r: MatchResult) {
         self.result.push(r)
     }
 
 }
 
-
+/// Represents letter match result.
 #[derive(Clone, Copy, Eq, PartialEq, Debug)]
 pub enum MatchResult {
-    MATCH,
-    EXISTS,
-    NONE
+
+    /// The guessed letter is in the secret
+    /// word at the correctly guessed position.
+    Match,
+
+    /// Letter has been guessed correctly,
+    /// but its position hasn't.
+    Exists,
+
+    /// There is no such letter in the secret word.
+    None
+
 }
 
 
 
+
+
+
 mod test {
-    use crate::rules::{GuessResult, MatchResult, Word};
+    use std::io::stdin;
+    use crate::game::{GuessResult, MatchResult, Word};
 
     #[test]
     fn guess_result_full_match() {
         let result = vec![
-            MatchResult::MATCH,
-            MatchResult::MATCH,
-            MatchResult::MATCH,
-            MatchResult::MATCH,
-            MatchResult::MATCH
+            MatchResult::Match,
+            MatchResult::Match,
+            MatchResult::Match,
+            MatchResult::Match,
+            MatchResult::Match
         ];
 
         let guess = GuessResult { result };
@@ -300,23 +433,23 @@ mod test {
         assert!(!result.full_match());
 
         assert_eq!(
-            MatchResult::MATCH,
+            MatchResult::Match,
             *result.result.get(0).unwrap()
         );
         assert_eq!(
-            MatchResult::NONE,
+            MatchResult::None,
             *result.result.get(1).unwrap()
         );
         assert_eq!(
-            MatchResult::EXISTS,
+            MatchResult::Exists,
             *result.result.get(2).unwrap()
         );
         assert_eq!(
-            MatchResult::NONE,
+            MatchResult::None,
             *result.result.get(3).unwrap()
         );
         assert_eq!(
-            MatchResult::NONE,
+            MatchResult::None,
             *result.result.get(4).unwrap()
         );
     }
